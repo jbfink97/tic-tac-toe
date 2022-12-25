7b5ede19dad873d40e7fcef5ec070d6015ac0f02
Johnny <johnnyfink97@gmail.com>
2022-12-24 22:46:31 -0500

    brainstormed with factory functions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', () => {
 
+    const playerX = document.querySelector('.X');
+    const playerO = document.querySelector('.O');
     const resetButton = document.getElementById('reset');
+    let activePlayer = 'X';
+    playerX.classList.add('active');
     const winning_combos = [
         [0, 1, 2],
         [3, 4, 5],
@@ -12,6 +16,24 @@ window.addEventListener('load', () => {
         [2, 4, 6]
     ]
 
+
+    const player = (shape) => {
+        const takeTurn = () => {
+            if (shape == 'X') {
+                cell.textContent = 'X';
+            }
+        }
+        const active = () => {
+            if (playerX.classList.contains('active')){
+                playerX.classList.remove('active');
+                playerO.classList.add('active');
+            } else {
+                playerO.classList.remove('active');
+                playerX.classList.add('active');
+            }
+        }
+    }
+
     const gameCells = document.querySelectorAll('.gameSquare');
 
     resetButton.addEventListener('click', () => {
@@ -19,16 +41,27 @@ window.addEventListener('load', () => {
             cell.textContent = '';
             if (cell.classList.contains('played')) cell.classList.remove('played');
         })
+        activePlayer = 'X';
+        playerO.classList.remove('active');
+        playerX.classList.add('active');
     })
 
     gameCells.forEach(cell => {
         cell.addEventListener('click', () => {
-            if (cell.textContent.length == 0) {
+            if (cell.classList.contains('played')) {
+                
+            } else if (player == 'X') {
                 cell.textContent = 'X';
                 cell.classList.add('played');
+                playerX.classList.remove('active');
+                playerO.classList.add('active');
+                activePlayer = 'O';
             } else {
-                cell.textContent = '';
-                cell.classList.remove('played');
+                cell.textContent = 'O';
+                cell.classList.add('played');
+                playerO.classList.remove('active');
+                playerX.classList.add('active');
+                activePlayer = 'X';
             }
         })
     })
